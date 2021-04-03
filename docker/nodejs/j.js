ae309d7fd0955fe1b10b704a4bd55a2af8c2a4ff
Nur İlayda Mert <57589311+ilaydamert@users.noreply.github.com>
2021-04-03 23:49:04 +0300

    Add files via upload

---
+++ docker/nodejs/j.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var options = {method: 'GET' }
+
+
+var server = http.createServer(function (req, res) {
+    if(req.url==='/staj'){
+        res.write('Hello world from Node.JS!')
+        res.end();
+    }
+    if(req.url==='/nodejs'){
+        res.write('Hello world from Node.JS!')
+        res.end();
+    }
+}).listen(5555)
